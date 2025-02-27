4baec2d73d4afa98805f143639ba96c5cb2c6dcc
Brian Chukwuisiocha <brianchukwuisiocha@gmail.com>
2025-02-27 11:40:49 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Brian Chukwuisiocha on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
